a17d26d4615552e036bd7064f749cc704544ecf3
HassanKhan123 <hk2602809@gmail.com>
2021-10-23 17:32:44 +0500

    added: palindrome solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // Solution 1
+  //   let reversedStr = str
+  //     .split('')
+  //     .reduce((reversed, character) => character + reversed, '');
+  //   return reversedStr == str;
+
+  return str.split('').every((char, i) => char == str[str.length - i - 1]);
+}
 
 module.exports = palindrome;
