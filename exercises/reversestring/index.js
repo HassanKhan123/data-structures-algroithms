6bfccf14cf0ec0b86cd8be7bf02ad4aa74dcbd2d
HassanKhan123 <hk2602809@gmail.com>
2021-10-23 15:20:55 +0500

    added: reverse string solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // Solution 1
+  // return str.split('').reverse().join('');
+
+  // Solution 2
+  // let reverseStr = '';
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //   reverseStr += str[i];
+  // }
+
+  // return reverseStr;
+
+  // Solution 3
+  let reverseStr = '';
+  for (let characters of str) {
+    reverseStr = characters + reverseStr;
+  }
+  return reverseStr;
+}
 
 module.exports = reverse;
